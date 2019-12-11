24cf806eee4251dd6907a7cc9d02d6efbf1ee3d5
Shauna <56253091+thesemach1nes@users.noreply.github.com>
2019-12-11 12:52:02 +0100

    Uitlezen van json file en oefenen van paralax

--- js/rocket.js
+++ js/rocket.js
@@ -1,12 +1,30 @@
 var timer = null;
 var countdownNumber = 10;
+var score = 0;
+var highscore = 0;
+var changeState = null;
+var state;
+
+var setState = state;
+
+function scoreboard(){
+	console.log('KAKDING');
+	if (state == 3) {
+	console.log('KAKDING2');
+ 		score = score+1;
+ 		document.getElementById('highscore').innerHTML = score;
+ 		console.log('randomNumber:', randomNumber, 'highscore:', highscore, "state:",  state);
+
+	}
+};
 
 var changeState = function (state){
 	document.body.className = 'body-state' + state;
 	clearInterval(timer);
 	countdownNumber = 10;
+	score = 0;
 	document.getElementById('countdown').innerHTML = countdownNumber;
-
+	document.getElementById('highscore').innerHTML = score;
 
 //countdown
 	if (state == 2) {
@@ -16,6 +34,7 @@ var changeState = function (state){
 				if (countdownNumber > 5 && countdownNumber <= 8) {
 					//preparing thrusters
 					document.getElementById('thrusters').className = 'thrusters show';
+
 				} else{
 					document.getElementById('thrusters').className = 'thrusters';
 				}
@@ -29,73 +48,82 @@ var changeState = function (state){
 				}
 
 				if (countdownNumber <= 0) {
-					changeState(3);
+					changeState(3);					
 				}
 		}, 500);
 	
 	} else if (state == 3) {
+		scoreboard();
 		var success = setTimeout(function ()
 			{
-				var randomNumber = Math.round(Math.random()*10);
-					console.log('randomNumber:', randomNumber );
-
+/*				var randomNumber = Math.round(Math.random()*10);
+*/					console.log('randomNumber:', randomNumber, 'highscore:', highscore, "state:",  state);
+				
 				//succes	
-				if (randomNumber > 5) {
+/*				if (randomNumber > 5) {
 					changeState(4);
 				}else{
 					changeState(5);
-				}
+				}*/
 			}, 5000);
 	};
 };
 
-
-
-//check mouse move voor parallax
-var input = {
-	mouseX: {
-		start:997,
-		end: window.innerWidth,
-		current: 0,
-	},
-	mouseY: {
-		start:0,
-		end: window.innerHeight,
-		current: 0,
-	}
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var myArr = JSON.parse(this.responseText);
+    document.getElementById("demo").innerHTML = myArr[0];
+  }
 };
+xmlhttp.open("GET", "sommen.JSON", true);
+xmlhttp.send();
 
-input.mouseX.range = input.mouseX.end - input.mouseX.start;
-input.mouseY.range = input.mouseY.end - input.mouseY.start;
+// //check mouse move voor parallax
+// var input = {
+// 	mouseX: {
+// 		start:997,
+// 		end: window.innerWidth,
+// 		current: 0,
+// 	},
+// 	mouseY: {
+// 		start:0,
+// 		end: window.innerHeight,
+// 		current: 0,
+// 	}
+// };
 
-
-var handleMouseMove = function(event){
-	input.mouseX.current = event.clientX;
-	input.mouseX.fraction = (input.mouseX.current - input.mouseX.start ) / input.mouseX.range;
-
-	input.mouseY.current = event.clientY;
-	input.mouseY.fraction = (input.mouseY.current - input.mouseY.start ) / input.mouseY.range;
-
-	if (input.mouseX.fraction < 0) {
-	input.mouseX.fraction = 0;
- }
-
- console.log('fraction X' , input.mouseX.fraction);
- console.log('fraction Y' , input.mouseY.fraction);
-
-
-
-}
+// input.mouseX.range = input.mouseX.end - input.mouseX.start;
+// input.mouseY.range = input.mouseY.end - input.mouseY.start;
 
 
+// var handleMouseMove = function(event){
+// 	input.mouseX.current = event.clientX;
+// 	input.mouseX.fraction = (input.mouseX.current - input.mouseX.start ) / input.mouseX.range;
 
-var handleResize = function(){
-	input.mouseX.end = window.innerWidth;
-	input.mouseX.range = input.mouseX.end - input.mouseX.start;
+// 	input.mouseY.current = event.clientY;
+// 	input.mouseY.fraction = (input.mouseY.current - input.mouseY.start ) / input.mouseY.range;
 
-	input.mouseY.end = window.innerHeight;
-	input.mouseY.range = input.mouseY.end - input.mouseY.start;
-}
+// 	if (input.mouseX.fraction < 0) {
+// 	input.mouseX.fraction = 0;
+//  }
 
-window.addEventListener("mousemove", handleMouseMove);
-window.addEventListener("resize", handleResize);
+//  console.log('fraction X' , input.mouseX.fraction);
+//  console.log('fraction Y' , input.mouseY.fraction);
+
+
+
+// }
+
+
+
+// var handleResize = function(){
+// 	input.mouseX.end = window.innerWidth;
+// 	input.mouseX.range = input.mouseX.end - input.mouseX.start;
+
+// 	input.mouseY.end = window.innerHeight;
+// 	input.mouseY.range = input.mouseY.end - input.mouseY.start;
+// }
+
+// window.addEventListener("mousemove", handleMouseMove);
+// window.addEventListener("resize", handleResize);
--- js/script.js
+++ js/script.js
@@ -40,3 +40,14 @@ var car = {
 	}
 	};
 	console.log('hello there');
+
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var myArr = JSON.parse(this.responseText);
+    document.getElementById("demo").innerHTML = myArr[0];
+  }
+};
+xmlhttp.open("GET", "sommen.JSON", true);
+xmlhttp.send();
